4f9c0bae07d5504ffb940dda6bbb02250153322e
Thomas B Wild <wild566@constance-e.pnl.gov>
2019-07-02 09:12:36 -0700

    new reference scenario files for Uruguay and Colombia, as well as updated config file that loads those xml files

---
+++ exe/run-gcam.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -A IDB_nexus
+#SBATCH -t 180
+#SBATCH -N 1
+ 
+job=$SLURM_JOB_NAME
+
+module load git
+module load svn/1.8.13
+module load R/3.4.3
+module load java/1.8.0_31
+module load gcc/8.1.0
+ 
+echo 'Library config:'
+ldd ./gcam.exe
+ 
+date
+time ./gcam.exe -C$job.xml -Llog_conf.xml
+date
